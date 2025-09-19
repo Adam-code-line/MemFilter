89e27bdc027618abf056d9473587c7292ab7d077
Adam-code-line <1319568366@qq.com>
2025-09-20 00:32:49 +0800

    创建项目文件结构

---
+++ app/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <LayoutHeader />
+
+    <slot />
+
+    <CommonButton />
+
+    <LayoutFooter />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
